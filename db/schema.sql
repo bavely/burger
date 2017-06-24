d359c46596a02751730c4d9960f315f49b4dfaa8
Bavely\Bavle <bavlesamy@gmail.com>
2017-06-24 09:34:04 -0700

    test

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ USE burgers_db;
 
 CREATE TABLE burgers(
     id INT(10) AUTO_INCREMENT NOT null,
-    burger_name VARCHAR(50) NOT null,
-    devoured BOOLEAN NOT null,
+    burger_name VARCHAR(100) NOT null,
+    devoured BOOLEAN DEFAULT false,
     ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
 
-var port = process.env.PORT || 3000;;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
