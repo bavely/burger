071975be3c4bb9dc47b7baf53d7c5f81e4b9d2ec
Bavely\Bavle <bavlesamy@gmail.com>
2017-06-23 22:30:19 -0700

    conn

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 
 var mysql = require('mysql');
-
+if(process.env.JAWDB_URL){
+  var connection = mysql.createConnection(process.env.JAWDB_URL);
+}
+else{
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
 });
-
+};
 connection.connect(function(err) {
     if (err) throw err;
 });
